487c27a1547fc31230f7fc275d2473a583907604
Layne <layneingramtaylor@gmail.com>
2019-05-01 21:10:56 -0500

    in the middle of fs.readFile

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,4 +2,28 @@ const fs = require("fs");
 const path = require("path");
 
 //create folder
-fs.mkdir();
+// fs.mkdir(path.join(__dirname, "/test"), {}, err => {
+//   if (err) throw err;
+//   console.log("folder created...");
+// });
+
+// create and write to file
+// fs.writeFile(path.join(__dirname, "/test", "hello.txt"), "hello world", err => {
+//   if (err) throw err;
+//   console.log("file written to...");
+//   //File append
+//   fs.appendFile(
+//     path.join(__dirname, "/test", "hello.txt"),
+//     "i love Node.js",
+//     err => {
+//       if (err) throw err;
+//       console.log("file written to...");
+//     }
+//   );
+// });
+
+//read file
+fs.readFile(path.join(__dirname, "test", "hello.txt"), utf8, err => {
+  if (err) throw err;
+  console.log("");
+});
