e30d0beb8b5eb7ad7f7e8c08168f1f6f068b1040
laynet <layneingramtaylor@gmail.com>
2019-05-07 12:37:39 -0500

    console.log'd the url being run

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const Person = require("./person");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const person1 = new Person("john doe", 30);
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+});
 
-person1.greeting();
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
